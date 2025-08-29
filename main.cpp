d3d36ace7607dd356b3d0cc941f4cc01c215142b
DenisLyakhov <denislyakhov2015@gmail.com>
2025-08-29 14:50:49 +0200

    create basic player representation

--- main.cpp
+++ main.cpp
@@ -2,12 +2,28 @@
 #include <GL/glu.h>
 #include <GL/GL.h>
 
+float playerX;
+float playerY;
+
+void drawPlayer() {
+    glColor3f(1, 1, 0);
+    glPointSize(8);
+    glBegin(GL_POINTS);
+    glVertex2i(playerX, playerY);
+    glEnd();
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    drawPlayer();
     glutSwapBuffers();
 }
 
 void init() {
+
+    playerX = 400;
+    playerY = 300;
+
     glClearColor(0.3, 0.3, 0.3, 0);
     gluOrtho2D(0, 800, 600, 0);
 }
